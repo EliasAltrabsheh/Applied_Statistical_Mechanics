349f3150bc1dec8ca0cda2f650cd5221b82ce0e2
Elias Altrabsheh <elias.altrabsheh@IQVIA.com>
2019-12-18 12:22:42 +0000

    updated week 6 assignments

--- Week6/Assignment/B2.py
+++ Week6/Assignment/B2.py
@@ -1,8 +1,5 @@
 import math, random, pylab
 
-def rho_free(x, y, beta):
-    return math.exp(-(x - y) ** 2 / (2.0 * beta))
-
 def levy_harmonic_path(xstart, xend, dtau, N):
     x = [xstart]
     for k in range(1, N):
@@ -15,31 +12,18 @@ def levy_harmonic_path(xstart, xend, dtau, N):
                1.0 / math.sqrt(Ups1)))
     return x
 
-
 beta = 20.0
-N = 80
+N =  80
+Ncut = N / 2
 dtau = beta / N
-delta = 1.0
-n_steps = 4000000
+n_steps = 100000
 x = [5.0] * N
 data = []
 for step in range(n_steps):
+    x = levy_harmonic_path(x[0], x[0], dtau, N)
+    x = x[Ncut:] + x[:Ncut]
     k = random.randint(0, N - 1)
-    knext, kprev = (k + 1) % N, (k - 1) % N
-    x_new = x[k] + levy_harmonic_path(x[0], x[0], dtau, N)
-    old_weight  = (rho_free(x[knext], x[k], dtau) *
-                   rho_free(x[k], x[kprev], dtau) *
-                   math.exp(-0.5 * dtau * x[k] ** 2))
-    new_weight  = (rho_free(x[knext], x_new, dtau) *
-                   rho_free(x_new, x[kprev], dtau) *
-                   math.exp(-0.5 * dtau * x_new ** 2))
-    if random.uniform(0.0, 1.0) < new_weight / old_weight:
-        x[k] = x_new
-    if step % N == 0:
-        k = random.randint(0, N - 1)
-        data.append(x[k])
-
-
+    data.append(x[k])
 
 pylab.hist(data, normed=True, bins=100, label='QMC')
 list_x = [0.1 * a for a in range (-30, 31)]
@@ -49,7 +33,7 @@ pylab.plot(list_x, list_y, label='analytic')
 pylab.legend()
 pylab.xlabel('$x$')
 pylab.ylabel('$\\pi(x)$ (normalized)')
-pylab.title('naive_harmonic_path (beta=%s, N=%i)' % (beta, N))
+pylab.title('levy_harmonic + "roll over" (beta=%s, N=%i)' % (beta, N))
 pylab.xlim(-2, 2)
-pylab.savefig('plot_B1_beta%s.png' % beta)
+pylab.savefig('plot_B2_beta%s.png' % beta)
 pylab.show()
