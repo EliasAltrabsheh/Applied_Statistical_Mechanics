618331b3305096d23eb13bcbb6211e075357bd2c
Elias Altrabsheh <elias.altrabsheh@IQVIA.com>
2019-12-19 16:50:07 +0000

    update code

--- Week6/Assignment/A1.py
+++ Week6/Assignment/A1.py
@@ -1,20 +1,25 @@
 import random, math, pylab
 
+
+
 def gauss_cut():
     while True:
         x = random.gauss(0.0, 1.0)
         if abs(x) <= 1.0:
             return x
-
 alpha = 0.5
 nsamples = 1000000
 samples_x = []
 samples_y = []
-for sample in range(nsamples):
+
+
+
+for sample in xrange(nsamples):
     while True:
-        x, y = gauss_cut(), gauss_cut()
-        p = math.exp(-alpha * (x ** 4 + y ** 4))
-        if random.uniform(0.0, 1.0) < p:
+        x = gauss_cut()
+        y = gauss_cut()
+        p = math.exp(-0.5 * (x ** 2 + y ** 2) - alpha * (x ** 4 + y ** 4))
+        if gauss_cut() < p:
             break
     samples_x.append(x)
     samples_y.append(y)
@@ -24,6 +29,6 @@ pylab.axis([-1.0, 1.0, -1.0, 1.0])
 cb = pylab.colorbar()
 pylab.xlabel('x')
 pylab.ylabel('y')
-pylab.title('A1_2')
-pylab.savefig('plot_A1_2.png')
+pylab.title('A1_1')
+pylab.savefig('plot_A1_1.png')
 pylab.show()
